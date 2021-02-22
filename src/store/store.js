2428ef6677995e6ff1a0cd0e0e44060943e65b69
khaledseleem0 <khaledseleem000@gmail.com>
2021-02-22 17:07:07 +0200

    Updates

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import Reduser from './reduser';
-const initalState  = {
-    cart:[]
-}
-const store  = createStore(Reduser,initalState); 
-export default store;
